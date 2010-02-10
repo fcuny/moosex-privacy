a2613e7ae5c3c9e8e78d8617a6669b723c2f5dd3
franck cuny <franck@lumberjaph.net>
2010-02-10 14:09:40 +0100

    meta class, with private and protected roles

---
+++ lib/MooseX/Privacy/Meta/Class/Private.pm
@@ -0,0 +1,30 @@
+package MooseX::Privacy::Meta::Class::Private;
+
+use Moose::Role;
+use MooseX::Types::Moose qw(Str ArrayRef );
+use MooseX::Privacy::Meta::Method::Private;
+
+has local_private_methods => (
+    traits     => ['Array'],
+    is         => 'ro',
+    isa        => ArrayRef [Str],
+    required   => 1,
+    default    => sub { [] },
+    auto_deref => 1,
+    handles    => { 'push_private_method' => 'push' },
+);
+
+sub add_private_method {
+    my ( $self, $method_name, $code ) = @_;
+    $self->add_method(
+        $method_name,
+        MooseX::Privacy::Meta::Method::Private->new(
+            name         => $method_name,
+            body         => $code,
+            package_name => $self->name
+        )
+    );
+    $self->push_private_method($method_name);
+}
+
+1;
---
+++ lib/MooseX/Privacy/Meta/Class/Protected.pm
@@ -0,0 +1,31 @@
+package MooseX::Privacy::Meta::Class::Protected;
+
+use Moose::Role;
+use MooseX::Types::Moose qw(Str ArrayRef );
+use MooseX::Privacy::Meta::Method::Protected;
+
+has local_protected_methods => (
+    traits     => ['Array'],
+    is         => 'ro',
+    isa        => ArrayRef [Str],
+    required   => 1,
+    default    => sub { [] },
+    auto_deref => 1,
+    handles    => { 'push_protected_method' => 'push' },
+);
+
+sub add_protected_method {
+    my ( $self, $method_name, $code ) = @_;
+    $self->add_method(
+        $method_name,
+        MooseX::Privacy::Meta::Method::Protected->new(
+            name         => $method_name,
+            body         => $code,
+            package_name => $self->name
+        )
+    );
+    $self->push_protected_method($method_name);
+}
+
+1;
+
