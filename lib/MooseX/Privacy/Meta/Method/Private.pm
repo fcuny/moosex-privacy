747d1c90e721d4d2632a4ee9529e820586a29d79
franck cuny <franck@lumberjaph.net>
2010-02-10 14:10:29 +0100

    generate private and protected methods

---
+++ lib/MooseX/Privacy/Meta/Method/Private.pm
@@ -0,0 +1,26 @@
+package MooseX::Privacy::Meta::Method::Private;
+
+use Moose;
+extends 'Moose::Meta::Method';
+
+use Carp;
+
+sub new {
+    my $class = shift;
+    my %args  = @_;
+
+    my $method       = delete $args{body};
+    my $private_code = sub {
+        croak "The "
+            . $args{package_name} . "::"
+            . $args{name}
+            . " name method is private"
+            unless ( scalar caller() ) eq $args{package_name};
+
+        goto &{$method};
+    };
+    $args{body} = $private_code;
+    $class->SUPER::wrap(%args);
+}
+
+1;
---
+++ lib/MooseX/Privacy/Meta/Method/Protected.pm
@@ -0,0 +1,29 @@
+package MooseX::Privacy::Meta::Method::Protected;
+
+use Moose;
+extends 'Moose::Meta::Method';
+
+use Carp;
+
+sub new {
+    my $class = shift;
+    my %args  = @_;
+
+    my $method         = delete $args{body};
+    my $protected_code = sub {
+        my $caller = caller();
+        croak "The "
+            . $args{package_name} . "::"
+            . $args{name}
+            . " name method is private"
+            unless $caller eq $args{package_name}
+                || $caller->isa( $args{package_name} );
+
+        goto &{$method};
+    };
+    $args{body} = $protected_code;
+    $class->SUPER::wrap(%args);
+}
+
+1;
+
