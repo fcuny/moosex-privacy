06fd12a0d8d71352b8de826b178d3b6ecee091c6
franck cuny <franck@lumberjaph.net>
2010-02-10 09:09:09 +0100

    initial commit

---
+++ lib/MooseX/Privacy.pm
@@ -0,0 +1,33 @@
+package MooseX::Privacy;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MooseX::Privacy -
+
+=head1 SYNOPSIS
+
+  use MooseX::Privacy;
+
+=head1 DESCRIPTION
+
+MooseX::Privacy is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck.cuny@rtgi.frE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
