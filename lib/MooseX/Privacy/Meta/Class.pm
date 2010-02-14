59e083364c260781e4a25e3f2160bab945c55dfa
franck cuny <franck@lumberjaph.net>
2010-02-14 12:08:23 +0100

    remove useless method

--- lib/MooseX/Privacy/Meta/Class.pm
+++ lib/MooseX/Privacy/Meta/Class.pm
@@ -6,12 +6,4 @@ use Moose::Meta::Class;
 with qw/MooseX::Privacy::Meta::Class::Private
     MooseX::Privacy::Meta::Class::Protected/;
 
-sub _build_meta_class {
-    my $self = shift;
-    return Moose::Meta::Class->create_anon_class(
-        superclasses => [ $self->method_metaclass ],
-        cache        => 1,
-    );
-}
-
 1;
