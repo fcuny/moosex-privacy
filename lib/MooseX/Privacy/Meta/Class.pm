c775b55331525a111495c27c5ad8f08a47820494
franck cuny <franck@lumberjaph.net>
2010-02-14 17:59:31 +0100

    add POD

--- lib/MooseX/Privacy/Meta/Class.pm
+++ lib/MooseX/Privacy/Meta/Class.pm
@@ -7,3 +7,23 @@ with qw/MooseX::Privacy::Meta::Class::Private
     MooseX::Privacy::Meta::Class::Protected/;
 
 1;
+__END__
+
+=head1 NAME
+
+MooseXMooseX::Privacy::Meta::Class
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck@lumberjaph.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
