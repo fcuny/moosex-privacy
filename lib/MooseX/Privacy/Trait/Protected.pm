9877be34968be4bcd831c28d09a96887acc715b9
franck cuny <franck@lumberjaph.net>
2010-04-29 14:07:28 +0200

    parameterized role for private and protected trait

--- lib/MooseX/Privacy/Trait/Protected.pm
+++ lib/MooseX/Privacy/Trait/Protected.pm
@@ -1,16 +1,7 @@
 package MooseX::Privacy::Trait::Protected;
 
 use Moose::Role;
-
-around accessor_metaclass => sub {
-    my ( $orig, $self, @rest ) = @_;
-
-    return Moose::Meta::Class->create_anon_class(
-        superclasses => [ $self->$orig(@_) ],
-        roles        => ['MooseX::Privacy::Meta::Attribute::Protected'],
-        cache        => 1
-    )->name;
-};
+with 'MooseX::Privacy::Trait::Role' => {name => 'protected'};
 
 1;
 
---
+++ lib/MooseX/Privacy/Trait/Role.pm
@@ -0,0 +1,22 @@
+package MooseX::Privacy::Trait::Role;
+
+use MooseX::Role::Parameterized;
+
+parameter name => ( isa => 'Str', required => 1, );
+
+role {
+    my $p         = shift;
+    my $role_name = "MooseX::Privacy::Meta::Attribute::" . ucfirst($p->name);
+
+    around accessor_metaclass => sub {
+        my ( $orig, $self, @rest ) = @_;
+
+        return Moose::Meta::Class->create_anon_class(
+            superclasses => [ $self->$orig(@_) ],
+            roles        => [$role_name],
+            cache        => 1
+        )->name;
+    };
+};
+
+1;
