40ffc155a8293bf5b110fea4bb26314ec652699b
franck cuny <franck@lumberjaph.net>
2010-04-29 14:08:35 +0200

    POD

--- lib/MooseX/Privacy/Trait/Role.pm
+++ lib/MooseX/Privacy/Trait/Role.pm
@@ -20,3 +20,25 @@ role {
 };
 
 1;
+__END__
+
+=head1 NAME
+
+MooseX::Privacy::Trait::Role
+
+=head1 SYNOPSIS
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck@lumberjaph.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
